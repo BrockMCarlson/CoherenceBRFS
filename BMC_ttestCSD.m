20298e73bc82c5b297f27516e90948aee79e2328
Brock <brock.m.carlson@vanderbilt.edu>
2018-11-15 14:20:23 -0600

    ttestCSD

--- BMC_ttestCSD.m
+++ BMC_ttestCSD.m
@@ -1,26 +1,11 @@
-%BMC plot CSD
-%loads a matlab workspace created in ReadInNS6 to use variables for
-%processing and then plotting. Available variables, 'EV.tp' 'Filename'
-%'LFP' 'MUA' and saveName'
-%trigger to stimulus on based on event codes
-%average LFP across trials
-    %run calcCSD
-    % convert uV to nA/mm^3
-    % subtract baseline
-    % 1/2 wave rectify
-    % pad out to same number of channels as LFP
-    % filter CSD
-    %plot
-
+%BMC CSD ttest
 clear
 close all
 cd 'E:\LaCie\DATA_KD\161005_E\'
-
 %load WorkspaceForProcessing_180705_evp001_test.mat
 load BRFSWorkspace_161005_E_brfs001ns2_2018-11-12.mat
 % Available variables, 'Cond' 'EV' 'extension' 'Filename' 'LFP' 'saveName'
 % and 'Unq_cond' 
-
 
 %% 4. PROCESS
 clear stimLFP
@@ -34,81 +19,54 @@ for tr = 1:length(EV.B) % trigger to stim-on times for all trials
     stimLFP(tr,:,:) = LFP(refwin,:);
 end
 
-% COMPUTE AVERAGE ACROSS TRIALS
-avgLFP = squeeze(mean(stimLFP,1));
-avgLFP_elC = avgLFP(:,1:32);
-
-% COMPUTE AVERAGE ACROSS TRIALS
-avgLFP = squeeze(mean(stimLFP,1));
-avgLFP_elC = avgLFP(:,1:24);
-avgLFP_elD = avgLFP(:,25:48);
-
-tvec = (-pre:post);
-figure(1),cla
-title('161005 elC CSDline');
-    for chanC = 1:size(avgLFP_elC,2)
-       plC = subplot(24,1,chanC);plot(tvec,avgLFP_elC(:,chanC));vline(0);hline(0);  
-    end
-    
-
-    figure(2),
-    title('161005 elD CSDline');
-    for chanD = 1:size(avgLFP_elD,2)
-        subplot(24,1,chanD);plot(tvec,avgLFP_elD(:,chanD));vline(0);hline(0);
-    end
+switchOrderStimLFP = permute(stimLFP,[2 3 1]);
+LFP_elC = switchOrderStimLFP(:,1:24,:);
+LFP_elD = switchOrderStimLFP(:,25:48,:);
 
 %% Get CSD
-CSD_elC = calcCSD(avgLFP_elC).*0.4; 
-CSD_elD = calcCSD(avgLFP_elD).*0.4;
-
-bl =1:50;
-bl_CSD_elC = mean(CSD_elC(:,bl),2);
-bl_CSD_elD = mean(CSD_elD(:,bl),2);
-blCSD_elC = (CSD_elC-bl_CSD_elC); 
-blCSD_elD = (CSD_elD-bl_CSD_elD);
+CSD_elC = calcCSD(LFP_elC).*0.4; 
+CSD_elD = calcCSD(LFP_elD).*0.4;
 
 gauss_sigma = 0.1;
-padCSD_elC = padarray(blCSD_elC,[1 0],NaN,'replicate');
-padCSD_elD = padarray(blCSD_elD,[1 0],NaN,'replicate');
-fCSD_elC = filterCSD(padCSD_elC(2:23,:),gauss_sigma);
-fCSD_elD = filterCSD(padCSD_elD(2:24,:),gauss_sigma);
+padCSD_elC = padarray(CSD_elC,[1 0],NaN,'replicate');
+padCSD_elD = padarray(CSD_elD,[1 0],NaN,'replicate');
 
-%% Plot CSD
+%% t-test each point in time after stimulus onset on each electrode, t-test
+% accross the trials.
+
+clear i j elC elD h
+collTstat = zeros(601,1);
+tResult = zeros(24,601);
+for i = 1:size(A.C,1) % performed per electrode contact
+    
+    contact_Bi = squeeze(A.D(i,:,:));
+    contact_dCOS = squeeze(B.D(i,:,61:122));
+    
+    for j = 1:size(contact_Bi,1)
+        Bi = contact_Bi(j,:);
+        dCOS = contact_dCOS(j,:);
+        [h,p,ci,stats] = ttest(Bi,dCOS);
+        collTstat(j) = stats.tstat;  
+    
+    end
+    
+    tResult(i,:) =  collTstat;
+    
+end
+
+%% Plot tResult
+pre = 100;   % pre-stim time (baseline) in ms
+post = 500; % post-stim time in ms
 tvec = (-pre:post);
-chanvec_elC = linspace(2,23,size(fCSD_elC_difer,2));
-chanvec_elD = linspace(2,24,size(fCSD_elD_difer,2));
+chanvec = size(tResult,1);
 
-figure(3)
-subplot(1,2,1); 
-imagesc(tvec,chanvec_elC,fCSD_elC_difer);
-colormap(flipud(jet)); colorbar; vline(0);
-mn1 = min(min(fCSD_elC_difer)); mx1 = max(max(fCSD_elC_difer));
+figure
+imagesc(tvec,1,tResult);
+colormap(jet); colorbar; vline(0);
+mn1 = min(min(tResult)); mx1 = max(max(tResult));
 maxval1 = max([abs(mn1) abs(mx1)]);
 caxis([-maxval1 maxval1]);
-title('161005 sinkbtm 15 electrode 1');
-
-subplot(1,2,2);
-imagesc(tvec,chanvec_elD,fCSD_elD_difer);
-colormap(flipud(jet)); colorbar; vline(0);
-mn2 = min(min(fCSD_elD_difer)); mx2 = max(max(fCSD_elD_difer)); 
-maxval2 = max([abs(mn2) abs(mx2)]);
-caxis([-maxval2 maxval2]);
-title('161005 sinkbtm 16 electrode 2');
-
-
-
-dateFormatOut = 'yyyy-mm-dd';
-saveDate = datestr(now,dateFormatOut);
-saveName = strcat('fig_CSD_',Filename,'_',saveDate);
-clear dateFormatOut saveDate
-saveas(figure(3),saveName)
-
-
-%CONGRATS... it finished
-load gong
-sound(y,Fs)
-
-
+title('t scores dCOS vs Bi on electrode D. 2nd half dCOS sample');
 
 %% Helper functions
 function CSD = calcCSD(avgLFP)
